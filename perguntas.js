832abdb26d197a9a377a52873ea19a57ba9a7ba8
mohamad <dson84975@gmail.com>
2024-11-28 20:57:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,27 @@
+criaCartao(
+    'programacao',
+    ' que e C++?',
+    'o C++ e uma linguagem de programacao'
+)
+criaCartao(
+   'Geografia',
+    'Qual a capital da EUA?',
+    'A capital da EUA é Washington'
+)
+
+criaCartao(
+    'programacao',
+    'o que e Java Script?',
+    'o java script e uma linguagem de programacao'
+)
+
+criaCartao(
+    'Programação 2.0',
+    '0 que é c#',
+    '0 c# é uma linguagem de programação'
+)
+criaCartao(
+    'lingua arabe',
+    'como se diz tudo bem em arabe?',
+    ' oi em arabe  e mahla'
+)
